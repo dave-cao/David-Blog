a67937f9501b7bca2896d2e4d03153ee8eb36faa
David Cao <dacao@protonmail.com>
2023-01-24 13:19:12 -0700

    added pull into git handle

--- git_handle.py
+++ git_handle.py
@@ -6,9 +6,12 @@ git_path = "."
 def git_push(commit_message):
     try:
         repo = Repo(git_path)
+        origin = repo.remote(name="origin")
+        origin.pull()
+        print("pulled")
+
         repo.git.add(all=True)
         repo.index.commit(commit_message)
-        origin = repo.remote(name="origin")
         origin.push()
         print("successfully pushed")
     except:
